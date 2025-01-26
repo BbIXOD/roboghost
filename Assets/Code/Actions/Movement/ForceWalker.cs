485ea5a3b9267141f5c299a2b1979ad9aef0a1b1
Maksym Diachok <maksdyachok2005@gmail.com>
2025-01-26 14:49:30 +0200

    Add force-based walking

---
+++ Assets/Code/Actions/Movement/ForceWalker.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+class ForceWalker : MonoBehaviour {
+    private Rigidbody2D _rb;
+
+    private void Start() {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    public float force = 1f;
+    public float maxSpeed = 10f;
+
+    public void Walk(Vector2 direction)
+    {
+        if (!Mathf.Approximately(direction.x, 0f))
+        {
+            var _destDirection = Mathf.Sign(direction.x);
+            var _destSpeed = _destDirection * force * Time.fixedDeltaTime;
+            _rb.AccelerateX(_destSpeed, maxSpeed);
+        }
+        else
+        {
+            var currentSpeed = _rb.linearVelocityX;
+            var deceleration = Mathf.Min(Mathf.Abs(currentSpeed) * _rb.mass, force * Time.fixedDeltaTime);
+            var decDirection = -Mathf.Sign(currentSpeed);
+
+            _rb.AccelerateX(deceleration * decDirection, maxSpeed);
+        }
+    }
+
+}
---
+++ Assets/Code/Utils/RigidbodyExtensions.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+static class RigidbodyExtensions {
+    public static void AccelerateX(this Rigidbody2D rb, float acceleration, float maxSpeed) {
+        rb.AddForce(Vector2.right * acceleration, ForceMode2D.Impulse);
+        rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -maxSpeed, maxSpeed);
+    }
+}
